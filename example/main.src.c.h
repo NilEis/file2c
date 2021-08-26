8d71dbe6b69c7b23dcc6b7f256382bee6cd4c9ff
NilEis <52820950+NilEis@users.noreply.github.com>
2021-08-26 15:17:09 +0200

    moved array in .c file

---
+++ example/main.src.c.h
@@ -0,0 +1,11 @@
+#ifndef main_src_test_H
+#define main_src_test_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+extern const uint8_t main_src_test[];
+
+extern const size_t main_src_test_size;
+
+#endif
